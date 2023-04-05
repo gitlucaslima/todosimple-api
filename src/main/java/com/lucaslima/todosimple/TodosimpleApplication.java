e0d90058e21af400e39d08e13c3e0304292180a2
Lucas Lima <57276011+gitlucaslima@users.noreply.github.com>
2023-04-05 10:40:19 -0500

    add usersModel, swagger documentation

--- src/main/java/com/lucaslima/todosimple/TodosimpleApplication.java
+++ src/main/java/com/lucaslima/todosimple/TodosimpleApplication.java
@@ -2,12 +2,34 @@ package com.lucaslima.todosimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "pessoas")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Peritos",
+		version = "1.0.0",
+		description = "Versão de teste de documentação"
+	)
+)
 public class TodosimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosimpleApplication.class, args);
+	}
+
+	@GetMapping(value  = "teste")
+	public String getTeste(){
+		
+		return "pessoa";
+
 	}
 
 }
---
+++ src/main/java/com/lucaslima/todosimple/models/User.java
@@ -0,0 +1,90 @@
+package com.lucaslima.todosimple.models;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "user";
+    
+    public interface CreateUser {};
+    public interface UpdateUser {};
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 150, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class , min=2, max = 100)
+    private String username;
+
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name= "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    private String password;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+
+    //  private List<Task> tasks = new ArrayList<Task>
+
+
+}

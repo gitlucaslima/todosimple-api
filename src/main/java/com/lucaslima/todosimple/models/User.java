ff29533d69b528205dced1d2e98e6c46498c170f
Lucas Lima <57276011+gitlucaslima@users.noreply.github.com>
2023-04-05 12:00:56 -0500

    add colum task

--- src/main/java/com/lucaslima/todosimple/models/User.java
+++ src/main/java/com/lucaslima/todosimple/models/User.java
@@ -1,88 +1,43 @@
 package com.lucaslima.todosimple.models;
 
-import java.util.List;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotEmpty;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Entity
-@Table(name= User.TABLE_NAME)
+@Table(name= "TB_USER")
 public class User {
-    public static final String TABLE_NAME = "user";
-    
-    public interface CreateUser {};
-    public interface UpdateUser {};
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", length = 150, nullable = false, unique = true)
-    @NotNull(groups = CreateUser.class)
-    @NotEmpty(groups = CreateUser.class)
-    @Size(groups = CreateUser.class , min=2, max = 100)
+    @NotBlank
+    @Column(name = "username", length = 150, unique = true)
     private String username;
 
-
-    @JsonProperty(access = Access.WRITE_ONLY)
-    @Column(name= "password", length = 60, nullable = false)
-    @NotNull(groups = {CreateUser.class, UpdateUser.class})
-    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    @NotBlank
+    @Column(name = "password", length = 60)
     private String password;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-
-        return result;
+    public User(Long id, @NotBlank String username, @NotBlank String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (username == null) {
-            if (other.username != null)
-                return false;
-        } else if (!username.equals(other.username))
-            return false;
-        if (password == null) {
-            if (other.password != null)
-                return false;
-        } else if (!password.equals(other.password))
-            return false;
-        return true;
-    }
+    
+
+    
 
     //  private List<Task> tasks = new ArrayList<Task>
 
